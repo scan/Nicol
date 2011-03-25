8afa94ab130325d574781e0f1458e5c21b3e1418
Tom Streller <dusklykan@yahoo.co.uk>
2011-03-25 23:32:45 +0100

    Activated blending over depth tests

--- src/test/scala/com/scan/nicol/App.scala
+++ src/test/scala/com/scan/nicol/App.scala
@@ -2,10 +2,16 @@ package com.scan.nicol
 
 object App extends Game("Nicol example App", 800, 600) {
 
+  import scala.util.Random._
+
   val image = Image("sika.png")
 
+  val positions: Seq[(Float, Float)] = Seq.fill(1000)((nextInt(800), nextInt(600)))
+
   def update = {
-    draw(image)
+    for (p <- positions) {
+      draw(image, p._1, p._2)
+    }
 
     //sync(60)
   }
