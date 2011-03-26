f31725c17fe40aaecb110cd7248d912bbffdbba7
Tom Streller <dusklykan@yahoo.co.uk>
2011-03-26 04:31:24 +0100

    Images are now drawn using display lists

--- src/main/scala/com/scan/nicol/Image.scala
+++ src/main/scala/com/scan/nicol/Image.scala
@@ -12,6 +12,8 @@ sealed trait Image extends Immutable {
   def sub(r: Rect): Image
 
   def sub(x: Int, y: Int, w: Int, h: Int): Image = sub(Rect(x, y, w, h))
+
+  def draw(x: Float, y: Float)
 }
 
 object Image {
@@ -28,22 +30,57 @@ object Image {
     override def toString = ("<\"" + res + "\", [" + width + ", " + height + "]>")
 
     def sub(r: Rect): Image = new GLSubImage(res, layer, r)
+
+    private lazy val list = makeList(GLUtils.draw(Quads) {
+      colour(1, 1, 1)
+      texCoord(0, 0)
+      vertex(0, 0, layer)
+      texCoord(texture.width, 0)
+      vertex(texture.imageSize._1, 0, layer)
+      texCoord(texture.width, texture.height)
+      vertex(texture.imageSize._1, texture.imageSize._2, layer)
+      texCoord(0, texture.height)
+      vertex(0, texture.imageSize._2, layer)
+    })
+
+
+    def draw(x: Float, y: Float) = preserve {
+      texture.bind
+      translate(x, y)
+      list.call
+    }
   }
 
   private class GLSubImage(res: String, layer: Float, rect: Rect) extends GLImage(res, layer) {
-    lazy val tx = (rect.left.toFloat / texture.imageSize._1.toFloat) * texture.width
-    lazy val ty = (rect.top.toFloat / texture.imageSize._2.toFloat) * texture.height
+    private lazy val list = makeList(GLUtils.draw(Quads) {
+      val tx = (rect.left.toFloat / texture.imageSize._1.toFloat) * texture.width
+      val ty = (rect.top.toFloat / texture.imageSize._2.toFloat) * texture.height
 
-    lazy val tw = min((rect.right.toFloat / texture.imageSize._1.toFloat) * texture.width, 1)
-    lazy val th = min((rect.bottom.toFloat / texture.imageSize._2.toFloat) * texture.height, 1)
+      val tw = min((rect.right.toFloat / texture.imageSize._1.toFloat) * texture.width, 1)
+      val th = min((rect.bottom.toFloat / texture.imageSize._2.toFloat) * texture.height, 1)
+
+      colour(1, 1, 1)
+      texCoord(tx, ty)
+      vertex(0, 0, layer)
+      texCoord(tw, ty)
+      vertex(width, 0, layer)
+      texCoord(tw, th)
+      vertex(width, height, layer)
+      texCoord(tx, th)
+      vertex(0, height, layer)
+    })
 
     override def width = rect.width
 
     override def height = rect.height
 
-    override def toString = ("<\"" + res + "\", [" + width + ", " + height + "], [(" + tx + ", " + ty + "), (" + tw + ", " + th + ")]>")
-
     override def sub(r: Rect): Image = new GLSubImage(res, layer, Rect(rect.x + r.x, rect.y + r.y, min(r.width, rect.width), min(r.height, rect.height)))
+
+    override def draw(x: Float, y: Float) = preserve {
+      texture.bind
+      translate(x, y)
+      list.call
+    }
   }
 
   def apply(res: String): Image = new GLImage(res, 0)
@@ -53,37 +90,7 @@ object Image {
   def apply(tex: Texture, layer: Float = 0): Image = new GLImage(tex.resource, layer)
 
   implicit object ImageRenderer extends Renderer[Image] {
-    def draw(that: Image, x: Float, y: Float) = that match {
-      case img: GLSubImage => {
-        val (tx, ty, tw, th) = (img.tx, img.ty, img.tw, img.th)
-        img.texture.bind
-        GLUtils.draw(Quads) {
-          colour(1, 1, 1)
-          texCoord(tx, ty)
-          vertex(x, y, img.layer)
-          texCoord(tw, ty)
-          vertex(x + img.width, y, img.layer)
-          texCoord(tw, th)
-          vertex(x + img.width, y + img.height, img.layer)
-          texCoord(tx, th)
-          vertex(x, y + img.height, img.layer)
-        }
-      }
-      case img: GLImage => {
-        val texture = img.texture
-        texture.bind
-        GLUtils.draw(Quads) {
-          colour(1, 1, 1)
-          texCoord(0, 0)
-          vertex(x, y, img.layer)
-          texCoord(texture.width, 0)
-          vertex(x + texture.imageSize._1, y, img.layer)
-          texCoord(texture.width, texture.height)
-          vertex(x + texture.imageSize._1, y + texture.imageSize._2, img.layer)
-          texCoord(0, texture.height)
-          vertex(x, y + texture.imageSize._2, img.layer)
-        }
-      }
-    }
+    def draw(that: Image, x: Float, y: Float) = that.draw(x, y)
   }
+
 }
--- src/test/scala/com/scan/nicol/App.scala
+++ src/test/scala/com/scan/nicol/App.scala
@@ -4,14 +4,14 @@ object App extends Game("Nicol example App", 800, 600) {
 
   import scala.util.Random._
 
-  val image = Image("sika.png")
+  val image = Image("sika.png") sub (10, 10, 20, 20)
 
   val positions: Seq[(Float, Float)] = Seq.fill(1000)((nextInt(800), nextInt(600)))
 
   def update = {
-    for (p <- positions) {
+    positions.foreach(p => {
       draw(image, p._1, p._2)
-    }
+    })
 
     //sync(60)
   }
