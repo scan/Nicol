f0f7ff064880a233a58a470e356b3087e47e1bef
Tom Streller <dusklykan@yahoo.co.uk>
2011-04-20 23:08:39 +0200

    Tried to work on the tilemap loading structure

--- src/main/scala/com/scan/nicol/tiles/Tilemap.scala
+++ src/main/scala/com/scan/nicol/tiles/Tilemap.scala
@@ -17,12 +17,38 @@ class Tilemap(ls: Array[Layer]) extends Immutable with DrawableEntity {
 
 import scala.xml._
 
+
+/* TODO: Implement working loading function */
 object Tilemap {
   def apply(res: String): Tilemap = {
-    val url = classOf[Tilemap].getClassLoader.getResource(res)
-    val xml = XML.load(url)
+    throw new Exception("Not implemented yet")
 
-    if(((xml \ "map")(0) \ "@version").text != "1.0") throw new FormatNotSupportException("Not TMX V0.1")
+    val url = classOf[Tilemap].getClassLoader.getResource(res)
+    val root = XML.load(url)
+
+    if ((root \ "@version").text != "1.0") throw new FormatNotSupportException("Not TMX V0.1")
+
+    val width = (root \ "@width").text.toInt
+    val height = (root \ "@height").text.toInt
+
+    val tileset = {
+      val tw = (root \ "@tilewidth").text.toInt
+      val th = (root \ "@tileheight").text.toInt
+
+      if ((root \ "tileset").length != 1) throw new FormatNotSupportException("Only one tileset per map supported")
+      val node = (root \ "tileset")(0)
+      if ((root \ "@tilewidth").text != (node \ "@tilewidth").text || (root \ "@tileheight").text != (node \ "@tileheight").text) throw new FormatNotSupportException("Root tilesize and tileset size must be equal")
+
+      Tileset((node \ "@source").text, (tw, th))
+    }
+
+    val layers = for(node <- (root \ "layer")) yield {
+      val data = node \ "data"
+      (data \ "@compression").text match {
+        case "gzip" => {}
+        case _ => throw new FormatNotSupportException("Compression method not supported");
+      }
+    }
 
     null
   }
