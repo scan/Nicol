1da2372b760fda5db87fb52c57452bbeb4f18966
Tom Streller <dusklykan@yahoo.co.uk>
2011-05-16 22:52:18 +0200

    sbt tweaks

--- project/build/Nicol.scala
+++ project/build/Nicol.scala
@@ -3,5 +3,5 @@ import sbt._
 class NicolProject(info: ProjectInfo) extends LWJGLProject(info) with IdeaProject {
 	override def testMainClass = Some("com.scan.nicol.App")
 
-  val scalacheck = "org.scala-tools.testing" % "scalacheck_2.8.1" % "1.8" % "test"
+  val scalacheck = "org.scala-tools.testing" %% "scalacheck" % "1.8" % "test"
 }
