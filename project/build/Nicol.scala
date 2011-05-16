7a9a23e281e33744a7623c9a3d13bea57d3d8f59
Scán <dusklykan@yahoo.co.uk>
2011-05-16 12:18:59 +0200

    added JOrbis deps

--- project/build/Nicol.scala
+++ project/build/Nicol.scala
@@ -3,5 +3,8 @@ import sbt._
 class NicolProject(info: ProjectInfo) extends LWJGLProject(info) with IdeaProject {
 	override def testMainClass = Some("com.scan.nicol.App")
 
-  val scalacheck = "org.scala-tools.testing" % "scalacheck_2.8.1" % "1.8" % "test"
+	val scalacheck = "org.scala-tools.testing" %% "scalacheck" % "1.8" % "test"
+	
+	val javaNet = "JavaNet" at "http://download.java.net/maven/2/"	
+	val jorbis = "com.projectdarkstar.ext.jorbis" % "jorbis" % "0.0.17"
 }
