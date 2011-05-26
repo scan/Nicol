e379cf3372154e6f9433b0f388548881df40f834
Tom Streller <dusklykan@yahoo.co.uk>
2011-05-26 15:53:10 +0200

    Ooops, corrected now

--- src/main/scala/com/scan/nicol/Game.scala
+++
@@ -1,9 +0,0 @@
-package com.scan.nicol
-
-import scala.actors._
-
-abstract class Game(entry: Scene) extends App with Actor {
-  start
-
-  def act = entry.apply
-}
--- src/main/scala/com/scan/nicol/renderer/Renderer.scala
+++
@@ -1,5 +0,0 @@
-package com.scan.nicol.renderer
-
-trait Renderer[A] {
-  def draw(that: A, x: Float = 0, y: Float = 0, colour: (Float, Float, Float) = (1, 1, 1)): Unit
-}
