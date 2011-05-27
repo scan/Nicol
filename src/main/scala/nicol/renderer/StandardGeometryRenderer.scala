0d78a8c226faa467e41cbb6bdef90612c154c594
Tom Streller <dusklykan@yahoo.co.uk>
2011-05-27 19:53:49 +0200

    Preparing publish

--- src/main/scala/nicol/renderer/StandardGeometryRenderer.scala
+++ src/main/scala/nicol/renderer/StandardGeometryRenderer.scala
@@ -8,16 +8,12 @@ import GLUtils._
 trait StandardGeometryRenderer {
 
   implicit object LineRenderer extends Renderer[Line] {
-    def draw(that: Line, position: (Float, Float), rgb: (Float, Float, Float), rotation: Float, offset: (Float, Float)) = preserve(withoutTextures {
-      translate(position._1, position._2)
-      rotate(rotation)
-      translate(offset._1, offset._2)
-      GLUtils.draw(Lines) {
+    def draw(that: Line, position: (Float, Float), rgb: (Float, Float, Float), rotation: Float, offset: (Float, Float)) =
+      stdDraw(position, rotation, offset)(GLUtils.draw(Lines) {
         colour(rgb._1, rgb._2, rgb._3)
         vertex(that.start.x, that.start.y)
         vertex(that.end.x, that.end.y)
-      }
-    })
+      })
   }
 
   implicit object CircleRenderer extends Renderer[Circle] {
@@ -26,26 +22,15 @@ trait StandardGeometryRenderer {
 
     import scala.math.{cos, sin}
 
-    def draw(that: Circle, position: (Float, Float), rgb: (Float, Float, Float), rotation: Float, offset: (Float, Float)) = preserve(withoutTextures {
-      translate(that.center.x + position._1, that.center.y + position._2)
-      rotate(rotation)
-      translate(offset._1, offset._2)
-      GLUtils.draw(LineLoop) {
+    def draw(that: Circle, position: (Float, Float), rgb: (Float, Float, Float), rotation: Float, offset: (Float, Float)) =
+      stdDraw(position, rotation, offset)(GLUtils.draw(LineLoop) {
         colour(rgb._1, rgb._2, rgb._3)
-        val r = that.radius
-        angles.foreach {
-          a =>
-            vertex(r * cos(a).toFloat, r * sin(a).toFloat)
-        }
-      }
-    })
+        angles.foreach(a => vertex(that.center.x+that.radius * cos(a).toFloat, that.center.y+that.radius * sin(a).toFloat))
+      })
   }
 
   implicit object AABoxRenderer extends Renderer[AABox] {
-    def draw(that: AABox, position: (Float, Float), rgb: (Float, Float, Float), rotation: Float, offset: (Float, Float)) = preserve(withoutTextures {
-      translate(position._1, position._2)
-      rotate(rotation)
-      translate(offset._1, offset._2)
+    def draw(that: AABox, position: (Float, Float), rgb: (Float, Float, Float), rotation: Float, offset: (Float, Float)) = stdDraw(position, rotation, offset) {
       GLUtils.draw(LineLoop) {
         colour(rgb._1, rgb._2, rgb._3)
         vertex(that.left, that.top)
@@ -53,36 +38,29 @@ trait StandardGeometryRenderer {
         vertex(that.right, that.bottom)
         vertex(that.left, that.bottom)
       }
-    })
+    }
   }
 
   implicit object QuadRenderer extends Renderer[Quad] {
-    def draw(that: Quad, position: (Float, Float), rgb: (Float, Float, Float), rotation: Float, offset: (Float, Float)) = preserve(withoutTextures {
-      translate(position._1, position._2)
-      rotate(rotation)
-      translate(offset._1, offset._2)
-      GLUtils.draw(LineLoop) {
+    def draw(that: Quad, position: (Float, Float), rgb: (Float, Float, Float), rotation: Float, offset: (Float, Float)) =
+      stdDraw(position, rotation, offset)(GLUtils.draw(LineLoop) {
         colour(rgb._1, rgb._2, rgb._3)
         vertex(that.p1.x, that.p1.y)
         vertex(that.p2.x, that.p2.y)
         vertex(that.p3.x, that.p3.y)
         vertex(that.p4.x, that.p4.y)
-      }
-    })
+      })
   }
 
   object FilledQuadRenderer extends Renderer[Quad] {
-    def draw(that: Quad, position: (Float, Float), rgb: (Float, Float, Float), rotation: Float, offset: (Float, Float)) = preserve(withoutTextures {
-      translate(position._1, position._2)
-      rotate(rotation)
-      GLUtils.draw(Quads) {
+    def draw(that: Quad, position: (Float, Float), rgb: (Float, Float, Float), rotation: Float, offset: (Float, Float)) =
+      stdDraw(position, rotation, offset)(GLUtils.draw(Quads) {
         colour(rgb._1, rgb._2, rgb._3)
         vertex(that.p1.x, that.p1.y)
         vertex(that.p2.x, that.p2.y)
         vertex(that.p3.x, that.p3.y)
         vertex(that.p4.x, that.p4.y)
-      }
-    })
+      })
   }
 
   object FilledCircleRenderer extends Renderer[Circle] {
@@ -91,34 +69,29 @@ trait StandardGeometryRenderer {
 
     import scala.math.{cos, sin}
 
-    def draw(that: Circle, position: (Float, Float), rgb: (Float, Float, Float), rotation: Float, offset: (Float, Float)) = preserve(withoutTextures {
-      translate(that.center.x + position._1, that.center.y + position._2)
-      rotate(rotation)
-      translate(offset._1, offset._2)
-      GLUtils.draw(Polygon) {
+    def draw(that: Circle, position: (Float, Float), rgb: (Float, Float, Float), rotation: Float, offset: (Float, Float)) =
+      stdDraw(position, rotation, offset)(GLUtils.draw(Polygon) {
         colour(rgb._1, rgb._2, rgb._3)
-        val r = that.radius
-        for (i <- 0 until angles.length - 1) {
-          vertex(r * cos(angles(i)).toFloat, r * sin(angles(i)).toFloat)
-          vertex(r * cos(angles(i + 1)).toFloat, r * sin(angles(i + 1)).toFloat)
-        }
-      }
-    })
+        angles.foreach(a => vertex(that.center.x+that.radius * cos(a).toFloat, that.center.y+that.radius * sin(a).toFloat))
+      })
   }
 
   object FilledAABoxRenderer extends Renderer[AABox] {
-    def draw(that: AABox, position: (Float, Float), rgb: (Float, Float, Float), rotation: Float, offset: (Float, Float)) = preserve(withoutTextures {
-      translate(position._1, position._2)
-      rotate(rotation)
-      translate(offset._1, offset._2)
-      GLUtils.draw(Quads) {
+    def draw(that: AABox, position: (Float, Float), rgb: (Float, Float, Float), rotation: Float, offset: (Float, Float)) =
+      stdDraw(position, rotation, offset)(GLUtils.draw(Quads) {
         colour(rgb._1, rgb._2, rgb._3)
         vertex(that.left, that.top)
         vertex(that.right, that.top)
         vertex(that.right, that.bottom)
         vertex(that.left, that.bottom)
-      }
-    })
+      })
   }
 
+  private def stdDraw(pos: (Float, Float), rot: Float, off: (Float, Float))(body: => Unit) =
+    preserve(withoutTextures {
+      translate(pos._1, pos._2)
+      rotate(rot)
+      translate(off._1, off._2)
+      body
+    })
 }
