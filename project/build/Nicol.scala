11fea5029e2ecbfa64a78e6b07c9cea8fd5efe4d
Tom Streller <scan@tom-desktop.(none)>
2011-06-01 14:27:15 +0200

    Publishing stuffs

--- project/build/Nicol.scala
+++ project/build/Nicol.scala
@@ -1,7 +1,13 @@
 import sbt._
 
 class NicolProject(info: ProjectInfo) extends LWJGLProject(info) with IdeaProject {
-	override def testMainClass = Some("nicol.App")
+  override def testMainClass = Some("nicol.App")
 
   val scalacheck = "org.scala-tools.testing" %% "scalacheck" % "1.8" % "test"
+
+  override def managedStyle = ManagedStyle.Maven
+
+  val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
+
+  Credentials(Path.userHome / ".ivy2" / ".credentials", log)
 }
