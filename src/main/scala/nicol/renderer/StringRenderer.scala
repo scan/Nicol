d99dcb0d90d624ae4677aa3151a73db42469c054
unknown <Tom Streller@.(none)>
2011-06-09 14:48:52 +0200

    Finally corrected that typo

--- src/main/scala/nicol/renderer/StringRenderer.scala
+++ src/main/scala/nicol/renderer/StringRenderer.scala
@@ -11,15 +11,11 @@ trait StringRenderer {
   import Font.arial
 
   implicit object ArialStringRenderer extends StringRenderer(arial)
-
-  object ArialCenderedStringRenderer extends CenteredStringRenderer(arial)
-
-  object ArialRightStringRenderer extends RightStringRenderer(arial)
-
+  
   /**
    * Draws a string with the given font, boringly left-aligned.
    */
-  case class StringRenderer(font: Font) extends Renderer[String] {
+  case class StringRenderer(font: Font = arial) extends Renderer[String] {
     def draw(that: String, position: (Float, Float), rgb: (Float, Float, Float), rotation: Float, offset: (Float, Float)) =
       font.write(that, position, rgb, rotation)
   }
@@ -27,7 +23,7 @@ trait StringRenderer {
   /**
    * Centers the String around the given position
    */
-  case class CenteredStringRenderer(font: Font) extends Renderer[String] {
+  case class CenteredStringRenderer(font: Font = arial) extends Renderer[String] {
     def draw(that: String, position: (Float, Float), rgb: (Float, Float, Float), rotation: Float, offset: (Float, Float)) =
       font.write(that, (position._1 - font.stringWidth(that) / 2, position._2 - font.height / 2), rgb, rotation)
   }
@@ -35,7 +31,7 @@ trait StringRenderer {
   /**
    * Writes the String left from the given position
    */
-  case class RightStringRenderer(font: Font) extends Renderer[String] {
+  case class RightStringRenderer(font: Font = arial) extends Renderer[String] {
     def draw(that: String, position: (Float, Float), rgb: (Float, Float, Float), rotation: Float, offset: (Float, Float)) =
       font.write(that, (position._1 - font.stringWidth(that), position._2), rgb, rotation)
   }
