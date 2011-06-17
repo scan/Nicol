46bfc58dff3f83f6d7d956fa64473d5800a55429
unknown <Tom Streller@.(none)>
2011-06-17 21:02:15 +0200

    Change test path

--- project/Build.scala
+++ project/Build.scala
@@ -1,22 +1,21 @@
 import sbt._
-
 import Keys._
 
 object General {
-  val settings = Defaults.defaultSettings ++ Seq (
+  val settings = Defaults.defaultSettings ++ Seq(
     organization := "com.github.scan",
     version := "0.1.1",
-    publishTo := Some("name" at "url"),
-    credentials += Credentials(Path.userHome / ".ivy2" / ".credentials")
+    credentials += Credentials(Path.userHome / ".ivy2" / ".credentials"),
+    libraryDependencies += "org.scala-tools.testing" %% "scalacheck" % "1.9" % "test"
   )
 }
 
 object NicolEngine extends Build {
-  lazy val nicol = Project (
+  lazy val nicol = Project(
     "nicol", file("."), settings = General.settings
-  )  aggregate (nicolCore)
+  ) aggregate (nicolCore)
 
- lazy val nicolCore = Project (
+  lazy val nicolCore = Project(
     "nicol-core", file("core"), settings = General.settings
   )
 }
